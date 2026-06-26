942cc0e1ddab08b41a085731410f21689550bf59
shreehari-divate <shreeharidivate03@gmail.com>
2026-06-26 23:10:21 +0530

    added app config

---
+++ config/settings.py
@@ -0,0 +1,20 @@
+#APLLICATION CONFIGURATION
+
+#1] LLM CONFIG
+MODEL_NAME = "llama-3.3-70b-versatile"
+TEMPERATURE = 0
+MAX_SUMMARY_WORDS = 300
+
+
+#2] APPLICATION (STREAMLIT)
+APP_TITLE = "AI CONTENT SUMMARIZER"
+APP_ICON = "🤖"
+SUPPORTED_LANGUAGES = ["en","hi"]
+REQUEST_TIMEOUT = 30
+USER_AGENT = (
+    "Mozilla/5.0",
+    "(Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 "
+    "(KHTML, like Gecko) "
+    "Chrome/138.0 Safari/537.36"
+)
